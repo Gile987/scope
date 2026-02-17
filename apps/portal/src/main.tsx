c0820b46e9a5f9538c7436b2219ce09fe55d2868
Cedric Vidal <cedric.vidal@gmail.com>
2026-02-17 04:24:26 -0800

    chore(portal): update entrypoint comment

--- apps/portal/src/main.tsx
+++ apps/portal/src/main.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-// Portal application entrypoint
+// Scope MT Portal - Application entrypoint
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
