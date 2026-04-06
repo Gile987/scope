6f982d5e09b357a3671e8ac5699be4dd5ae857ba
Cedric Vidal <cedric.vidal@gmail.com>
2026-04-05 21:59:28 -0700

    chore: remove version field from Scenario type and queue-processor
    - Remove version from Scenario interface (shared types)
    - Remove scenarioVersion pass-through in queue-processor runMultiTurnLoop call

--- packages/shared/src/queue/queue-processor.ts
+++ packages/shared/src/queue/queue-processor.ts
@@ -370,7 +370,6 @@ export class CodingAgentQueueProcessor extends BaseQueueProcessor<RequestDocumen
       processor: this.processor,
       task: requestDoc.scenario.task,
       criteria: requestDoc.scenario.criteria,
-      scenarioVersion: requestDoc.scenario.version,
       maxIterations,
       judgeClient,
       blobStorage,
